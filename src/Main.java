d65d29927c0d3d7feed1176457477aae8ba00019
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-14 11:53:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    /*
+     * Aufgabenstellung:
+     * - Computer erstellt eine N-stellige Zufallszahl
+     *   (Bedingung: jede Ziffer kommt in dieser Zufallszahl nur 1x vor)
+     * - N kann vom Anwender eingegeben werden (aka Schwierigkeitsgrad)
+     *
+     * Raten:
+     * - Anwender gibt einen Tipp ab, d.h. gibt eine Zahl ein
+     * - Jede korrekte Ziffer an der richtigen Position gibt ein "+"
+     * - Jede Ziffer, die in der Zahl vorhanden ist, aber nicht an der
+     *   korrekte Position gibt ein "-"
+     * - Eine Ziffer, die nicht in der Zahl vorkommt, gibt keinen Hinweis
+     *
+     * - Wenn die Zahl der "+" dem N entspricht, wurde die Zahl korrekt
+     *   erraten
+     * - Am Ende soll die Anzahl der Rateversuche ausgegeben werden
+     */
+}
